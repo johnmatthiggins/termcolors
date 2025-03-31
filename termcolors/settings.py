f1ebc8148c8c7917f20ce81936ee6ef50088a03d
John Higgins <johnmatthiggins@gmail.com>
2025-03-31 05:54:37 -0700

    configured logger

--- termcolors/settings.py
+++ termcolors/settings.py
@@ -148,6 +148,20 @@ STORAGES = {
     },
 }
 
+LOGGING= {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    }
+}
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
