2f1a3537590b15edeff767cd774501b9793a3de0
John Higgins <johnmatthiggins@gmail.com>
2025-03-31 05:58:31 -0700

    fixed static file configuration

--- termcolors/settings.py
+++ termcolors/settings.py
@@ -148,7 +148,7 @@ STORAGES = {
     },
 }
 
-LOGGING= {
+LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {
@@ -165,7 +165,7 @@ LOGGING= {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = "staticfiles/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
